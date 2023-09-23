43d90e300bcfc60729e7df13bdb5f834c528f7e6
saddu <sadduman44@gmail.com>
2023-09-23 16:58:39 +0530

    first commit

---
+++ src/Converter/GFG.java
@@ -0,0 +1,14 @@
+package Converter;
+
+import javax.swing.JFrame;
+
+public class GFG {
+	//FUNCTION TO CONVERT RUPEE TO DOLLER AND ALSO
+	//DOLLER TO RUPPEE USING JAVA SWING
+	public static void converter() {
+		
+		//CREATE A FRAME FOR CURRENCY CONVERTER
+		JFrame frame = new JFrame("CURRENCY CONVERTER");
+	}
+
+}
