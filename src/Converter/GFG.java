b6be2cc5855c936ae2d69240d787254da83de464
saddu <sadduman44@gmail.com>
2023-09-23 20:39:39 +0530

    project done completed.

--- src/Converter/GFG.java
+++ src/Converter/GFG.java
@@ -1,5 +1,8 @@
 package Converter;
-import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -25,57 +28,97 @@ public class GFG {
 		l1.setBounds(20, 40, 150, 30);
 		
 		System.out.println("label one done");
-		frame.add(l1);
+
 		
 //		naming the labels l1 and setting the bounds
 		l2= new JLabel("DOLLER");
 		l2.setBounds(170, 40, 150, 30);
 	
 		System.out.println("label two also done");
-		frame.add(l2);
+
 		
 //		initializing the textfield by default zero and setting
 //		the bounds for that textfields.
 		
 		t1= new JTextField("0");
 		t1.setBounds(40,70,50,20);
-		frame.add(t1);
-		
+
+//		
 		 t2 = new JTextField("0");
 		t2.setBounds(170, 70, 50, 20);
-		frame.add(t2);
-		
-//		t2= new JTextField("0");
-//		t2.setBounds(150,70,50,20);
+
+
 		
 //		initializing buttons for indian ,amrican and also close the project
 		b1=new JButton("INR");
 		b1.setBounds(30, 100, 60, 15);
-		frame.add(b1);
+
 		
 		b2= new JButton("DOLLER");
 		b2.setBounds(150, 100, 100, 15);
-		frame.add(b2);
+
 		
 		b3= new JButton("CLOSE");
 		b3.setBounds(80, 140, 80, 20);
-		frame.add(b3);
-//		
-//		frame.add(t2);
-//		frame.add(t1);
-//		frame.add(l2);
-//		frame.add(l);
-//		frame.add(b3);
-//		frame.add(b2);
-//		frame.add(b1);
-	
+
 		
+		b1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			double d = Double.parseDouble(t1.getText());
+			
+			double d2 = d/65.25;
+			
+			String str1= String.valueOf(d2);
+			t2.setText(str1);
+			System.out.println("button one working fine");
+				
+			}
+		}); 
+		
+		b2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			double d3 =Double.parseDouble(t2.getText());
+			
+			double d4= d3*65.25;
+			
+			String str2 = String.valueOf(d4);
+			t1.setText(str2);
+			System.out.println("button two working fine");
+				
+			}
+		});	
+		b3.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("button three working fine");
+			frame.dispose();
+				
+			}
+		});
+		
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.out.println("window shotdone");
+				System.exit(0);
+			}
+		});
+		frame.add(l1);
+		frame.add(l2);
+		frame.add(t1);
+		frame.add(t2);
+		frame.add(b1);
+		frame.add(b2);
+		frame.add(b3);
 	
 		frame.setSize(400, 300);
 		frame.setVisible(true);
-	
 		frame.setLayout(null);
-//		frame.repaint();
+		frame.repaint();
 		
 	}
 
